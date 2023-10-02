b821a3b5946c4c80a64f15c66a731a4f29e1f17f
BintangLaura <bintanglaura222@gmail.com>
2023-10-02 21:52:47 +0700

    [FIX] tambah index (login), dashboard dan product untuk tugas 13

---
+++ pages/product.php
@@ -0,0 +1,356 @@
+<?php 
+    $gambar = [
+        "../assets/images/kemeja-flanel.jpg", "../assets/images/kemeja-zora.jpg", 
+        "../assets/images/basic-sweater.jpg", "../assets/images/kemeja-giovani.jpg", 
+        "../assets/images/Kaos-sablon.jpg", "../assets/images/jaket-bomber.jpg",
+        "../assets/images/kaos-polos.jpg", "../assets/images/kardigan-rajut.jpg",
+    ];
+
+    $nama = [
+        "Kemeja Flanel", "Kemeja Pria Zora", "Basic Sweater", "Kemeja Giovani",
+        "Kaos Sablon Pantai", "Jaket Bomber", "Kaos Polos", "Kardigan Rajut",
+    ];
+
+    $harga = [
+        "Rp. 89.000,-", "Rp. 59.000,-", "Rp. 40.000,-", "Rp. 35.000,-", "Rp. 25.000,-", "Rp. 125.000,-", "Rp. 25.000,-", "Rp. 50.000,-",
+    ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Halaman Daftar Produk Dengan Bootstrap dan PHP</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <!--<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">-->
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="../assets-bootstrap/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">-->
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="../assets-bootstrap/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="../assets-bootstrap/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="../assets-bootstrap/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="../assets-bootstrap/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="../assets-bootstrap/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="../assets-bootstrap/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="../assets-bootstrap/plugins/summernote/summernote-bs4.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <!-- <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="../assets-bootstrap/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+  </div> -->
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="../assets/images/Logo.jpg" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Fashione</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="../assets-bootstrap/img/avatar2.png" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Bintang Laura Anjani</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="../pages/dashboard.php" class="nav-link">
+                  <i class="nav-icon fas fa-tachometer-alt"></i>
+                  <p>
+                    Dashboard
+                  </p>
+                </a>
+              </li>
+            <li class="nav-item">
+                <a href="../pages/product.php" class="nav-link active">
+                  <i class="nav-icon fas fa-shopping-cart"></i>
+                  <p>
+                    Daftar Produk
+                   <!--<span class="right badge badge-danger">New</span>-->
+                  </p>
+                </a>
+              </li>
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Daftar Produk</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item">Dashboard</li>
+              <li class="breadcrumb-item active">Daftar Produk</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[0]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[0]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[0]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[1]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[1]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[1]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[2]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[2]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[2]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[3]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[3]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[3]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[4]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[4]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[4]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[5]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[5]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[5]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[6]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[6]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[6]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="card">
+              <div class="card-body">
+                <img src="<?php echo $gambar[7]?>" alt="Kemeja Flanel" width="100%">
+
+                <h4 class="text-center mt-2"><?php echo $nama[7]?></h4>
+                <h5 class="text-center mt-2"><?php echo $harga[7]?></h5>
+
+                <div class="row justify-content-center">
+                  <a href="#" class="btn btn-primary col-md-5">Detail</a>
+                  <a href="#" class="btn btn-success ml-2 col-md-5">Beli</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="../pages/dashboard.html">Fashione</a>.</strong>
+    All rights reserved.
+    <!-- <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>-->
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="../assets-bootstrap/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../assets-bootstrap/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="../assets-bootstrap/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="../assets-bootstrap/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="../assets-bootstrap/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="../assets-bootstrap/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="../assets-bootstrap/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="../assets-bootstrap/plugins/moment/moment.min.js"></script>
+<!-- daterangepicker -->
+<script src="../assets-bootstrap/plugins/moment/moment.min.js"></script>
+<script src="../assets-bootstrap/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../assets-bootstrap/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="../assets-bootstrap/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../assets-bootstrap/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../assets-bootstrap/js/adminlte(2).js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../assets-bootstrap/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="../assets-bootstrap/js/pages/dashboard.js"></script>
+</body>
+</html>
