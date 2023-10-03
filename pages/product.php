bb64e07843461caa27b4cec34e87db1da3ae1f60
BintangLaura <bintanglaura222@gmail.com>
2023-10-03 20:51:05 +0700

    [FIX] update dan tambah link sidebar

--- pages/product.php
+++ pages/product.php
@@ -140,6 +140,15 @@
                   </p>
                 </a>
               </li>
+            <li class="nav-item">
+                <a href="../pages/product-loop.php" class="nav-link">
+                  <i class="nav-icon fas fa-shopping-cart"></i>
+                  <p>
+                    Daftar Produk Dengan Looping
+                   <!--<span class="right badge badge-danger">New</span>-->
+                  </p>
+                </a>
+              </li>
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
         </ul>
