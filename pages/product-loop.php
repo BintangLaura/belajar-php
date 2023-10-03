a1c149988ec71f4e195ede3cc3874513da736c88
BintangLaura <bintanglaura222@gmail.com>
2023-10-03 20:56:37 +0700

    [FIX] ubah content header

--- pages/product-loop.php
+++ pages/product-loop.php
@@ -199,7 +199,7 @@
             <ol class="breadcrumb float-sm-right">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
               <li class="breadcrumb-item">Dashboard</li>
-              <li class="breadcrumb-item active">Daftar Produk</li>
+              <li class="breadcrumb-item active">Daftar Produk Looping</li>
             </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
