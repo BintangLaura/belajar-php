3c4326504760adda1939c9abf72ed25142153fe6
BintangLaura <bintanglaura222@gmail.com>
2023-10-08 21:27:05 +0700

    [FIX] menambahkan codingan di file logout

--- pages/logout.php
+++ pages/logout.php
@@ -1,5 +1,5 @@
 <?php
-// session_start();
+session_start();
 session_destroy();
 echo "<script>alert('Anda Telah Keluar Dari Aplikasi. Silahkan Login Kembali!'); window.location='../index.php'</script>";
 ?>
